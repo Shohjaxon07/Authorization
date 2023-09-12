1fe9840c716e671899070586069b70b9c0dfbb73
Shohjaxon07 <shmobilelegends@gmail.com>
2023-09-12 19:19:36 +0500

    Second commit

--- config/db.js
+++ config/db.js
@@ -5,16 +5,24 @@ const connectDB = async () => {
         useUnifiedTopology: true,
         
     })
-    .then(() => {
-        console.log('Connected to MongoDB Atlas');
-        // Start executing your code here
-      })
-      .catch((error) => {
-        console.error('Error connecting to MongoDB Atlas:', error);
-        // Handle the error here or throw it to be caught at a higher level
-        // throw error;
-      });
+    console.log(`connecting to MongoDB `)
 }
 
 module.exports = connectDB
 
+// const options = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   };
+  
+//   // Connect to MongoDB Atlas cluster
+//   mongoose.connect(uri, options)
+//     .then(() => {
+//       console.log('Connected to MongoDB Atlas');
+//       // Start executing your code here
+//     })
+//     .catch((error) => {
+//       console.error('Error connecting to MongoDB Atlas:', error);
+//       // Handle the error here or throw it to be caught at a higher level
+//       // throw error;
+//     });
